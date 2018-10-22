e1256dcd31beb5723425ebc3cd301123155a99a6
Luis Borjas <luisfborjas@gmail.com>
2018-10-21 21:30:09 -0400

    had to downgrade to solrj 4.4.0 to talk to my dev box

--- project.clj
+++ project.clj
@@ -6,8 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ; these older versions match our sad prod/dev java/solr
                  [com.rabbitmq/amqp-client "4.8.3"]
-                 ;[org.apache.solr/solr-core "5.5.0"]
-                 [org.apache.solr/solr-solrj "5.5.0"]]
+                  ; this bullshit is required by the http solr server:
+                 [commons-logging/commons-logging "1.1.1"]
+                 [org.apache.solr/solr-solrj "4.4.0"]]
   :main ^:skip-aot reindexer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/reindexer/solr.clj
+++ src/reindexer/solr.clj
@@ -1,28 +1,23 @@
 (ns reindexer.solr
   (:import [org.apache.solr.client.solrj SolrQuery]
-           [org.apache.solr.client.solrj.impl HttpSolrClient]
+           [org.apache.solr.client.solrj.impl HttpSolrServer]
            [org.apache.solr.common SolrInputDocument]))
 
-
-;; String urlString = "http://localhost:8983/solr/techproducts";
-;; SolrClient solr = new HttpSolrClient.Builder(urlString).build();
-; Have to add a localtunnel to my ssh config for a remote box:
-;; LocalForward 8081 solr:8080
 
 (defn connect-to-cores
   "Given a config map, connects to a remote solr instance via http"
   [urls]
-  (map #(HttpSolrClient. %) urls))
+  (map #(HttpSolrServer. %) urls))
 
 (defn query-solr
-  "Given a solr url and a query, do a simple select on it"
+  "Given a solr cxn and a query, return a collection of results"
   [client query-string]
   (let [query    (doto (SolrQuery.) (.setQuery query-string))
         response (.query client query)]
     (.getResults response)))
 
 (defn as-input-document
-  "Given a clojure map, return a solr input document"
+  "Given a clojure map with string keys, return a solr input document"
   [m]
   (let [document (SolrInputDocument.)]
     (doseq [[name value] m]
@@ -38,6 +33,8 @@
       (.commit c))))
 
 
+
+
 ;; Some notes
 ;; Had to tread carefully because solrj ifaces are always changing, thankfully
 ;; the javadocs + sorta current guides kinda helped:
@@ -45,19 +42,32 @@
 ;; * https://lucene.apache.org/solr/guide/6_6/using-solrj.html
 ;; * http://www.solrtutorial.com/solrj-tutorial.html
 
+; Have to add a localtunnel to my ssh config for a remote box:
+;; LocalForward 8081 solr:8080
+
+;; some REPL testing: I usually send forms one by one with C-c M-p
+;; but you can also just eval the whole thing with C-c M-e at the end of the
+;; do block.
+
 (comment
   (do
-    (def clients (connect-to-cores ["http://localhost:8081/solr440/products"]) )
+    (def clients (connect-to-cores ["http://127.0.0.1:8081/solr440/products"]))
     (def results (query-solr (first clients) "id:111"))
     (println (get (first results) "product_name"))
                                         ; => "blowPro Blow Up Thickening Mist
+    (def doc (first results))
     ;; the document is a SolrDocument instance, so, mutable
     (.setField doc "product_name" "Luis's magical product")
+    (.get doc "product_name")
     (def doc-map (zipmap (keys doc) (vals doc)))
+    ; notice it's a clojure map now:
+    (get doc-map "product_name")
     ;; one interesting to notice is that trying to print new-doc
     ;; to the REPL raises an exception. Gotta inspect it lightly
     (def new-doc (as-input-document doc-map))
     (.keySet new-doc)
     (.get new-doc "product_name")
-    (def doc-maps [doc-map])
-    ))
+    ;; notice that "title_sort" isn't supposed to be here, but that's a testing
+    ;; issue, so we just dissoc:
+    (def doc-maps [(dissoc doc-map "title_sort")])
+    (update-in-cores clients doc-maps)))
